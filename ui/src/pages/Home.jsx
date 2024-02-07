a9b56d7bcce94f67e0bed7f499580f560d65763b
devel@mauriciocap.com <devel@mauriciocap.com>
2024-02-07 11:00:28 -0500

    OK: max width for file viewer

--- ui/src/pages/Home.jsx
+++ ui/src/pages/Home.jsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import { FileSelector } from '../components/file-selector';
 
 export const Home= (props)=> {
-	return ( <>
+	return ( <div className="card" style={{maxWidth: '80rem', margin: 'auto'}}>
 		<FileSelector {...props} />
-	</>
+	</div>
 	)
 };
