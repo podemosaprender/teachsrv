d6b7430165132d52361401158ccb79c4db70a1d0
devel@mauriciocap.com <devel@mauriciocap.com>
2024-02-07 00:30:25 -0500

    OK: chunks

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -3,8 +3,22 @@ import react from '@vitejs/plugin-react'
 
 const BasePath= (process.env.GITHUB_REPOSITORY||'').replace(/^[^\/]*/,'') 
 
+function manualChunks(id) {
+	console.log("manualChunks",id);
+	if (id.includes('node_modules')) {
+		return (
+			id.includes('prime') ? 'vendor-prime' :
+			id.includes('mirror') ? 'vendor-mirror' :
+			'vendor'
+		)
+	}
+}
+
 //SEE: https://vitejs.dev/config/
 export default defineConfig({
 	base: BasePath,
   plugins: [react()],
+	build: {
+		rollupOptions: { output: { manualChunks }},
+	},
 })
