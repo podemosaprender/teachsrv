fe980e211e2aef9e29ac663802c554ec7916cf02
devel@mauriciocap.com <devel@mauriciocap.com>
2024-02-10 08:59:16 -0500

    WIP: generalize api, make tmux a special case

--- server/src/index.js
+++ server/src/index.js
@@ -1,23 +1,23 @@
 //INFO: CodeEditingServer: static ui, editing files, proxy to GUEST application
-//A: we add handlers to the express app, the order is relevant: the first succeeding ends the chain
+//U: we add handlers to the express app, the order is relevant: the first succeeding ends the chain
 
 const CFG_API_JS_PATH= process.argv[2] || process.env.API_JS_PATH || './api_base.js';
 console.log('API, loading from', CFG_API_JS_PATH);
 const { createApiInstance }= await import(CFG_API_JS_PATH);
 const api= createApiInstance();
+//A: we have the api that controls the EditedApp!
 
 const CFG_LISTEN_PORT= process.env.P_NET_GUEST_PORT0 || 3000;
 const CFG_LISTEN_INTERFACES= process.env.P_NET_INTERFACES || '0.0.0.0'; //SEC:ALL as default?
+const CFG_API_PFX= ''; //U: to avoid clashes with EDITED_APP //XXX:IMPLEMENT
 
 import { dir_for_this_script } from './lib.js';
 const CFG_STATIC_PATH= dir_for_this_script()+'/static_ui_generated'; //XXX:CFG
-const CFG_RESULT_DOMAIN_PFX= 'env_';
 console.log('CFG_STATIC_PATH', CFG_STATIC_PATH);
 
 import express from 'express';
 import cors from 'cors';
 import HttpProxy from 'http-proxy';
-
 
 const proxy= HttpProxy.createProxyServer({});
 
@@ -28,32 +28,14 @@ console.log("SERVER LISTENING ON", `${CFG_LISTEN_INTERFACES} e.g. http://localho
 app.use(cors()); //XXX:SEC:limitar!
 
 //S: PROXY {
-async function proxy_to_urlP(req, protocol) { //U: return a url to proxy to OR null if must not be proxied
-	let proxy_to_url= null; //DFLT
-
-	/* req.headers includes
-	 host: 'st1.test1.podemosaprender.org',
-  'x-forwarded-host': 'st1.test1.podemosaprender.org',
-  'forwarded-request-uri': '/src/pepe/App.jsx',
-	*/
-	const host= req.headers.host;
-	if (host.startsWith(CFG_RESULT_DOMAIN_PFX)) { 
-		const env_name_UNSAFE= host.split('.')[0].slice(CFG_RESULT_DOMAIN_PFX.length)
-		proxy_to_url= await api.env_app_url({env_name_UNSAFE});
-		console.log("PROXY", {protocol, app_url, host, req_url: req.originalUrl});
-	}
-
-	return proxy_url;
-}
-
 app.use(async (req, res, next) => { //A: proxy calls to EditedApp(s) and dependencies
-	const proxy_to_url= await proxy_to_urlP(req,'HTTP*');
+	const proxy_to_url= await api.proxy_to_urlP(req,'HTTP*');
 	if (proxy_to_url) { proxy.web(req, res, { target: proxy_to_url}) } //A: api says "proxy to this url"
 	else { next() }
 });
 
 server.on('upgrade', async (req, socket, head) => { //A: proxy calls to EditedApp(s) and dependencies (WebSockets)
-	const proxy_to_url= await proxy_to_urlP(req,'HTTP*');
+	const proxy_to_url= await api.proxy_to_urlP(req,'HTTP*');
 	if (proxy_to_url) { proxy.ws(req, socket, head,{ target: proxy_to_url}) }	//A: api says "proxy to this url"	
 	else { next() }
 });
@@ -66,7 +48,7 @@ app.use(express.static(CFG_STATIC_PATH)); //A: serve CodeEditingUI
 app.use(express.json());
 
 //S: EDITING FILE SERVER {
-app.get('/src/:env/*', async (req,res) => {
+app.get(CFG_API_PFX+'/src/:env/*', async (req,res) => {
 	const env_name_UNSAFE= req.params.env; 
 	const file_path_UNSAFE= req.params[0]; 
 	if (file_path_UNSAFE=='*') {
@@ -76,7 +58,7 @@ app.get('/src/:env/*', async (req,res) => {
 	}
 })
 
-app.post('/src/:env/*', async (req, res) => {
+app.post(CFG_API_PFX+'/src/:env/*', async (req, res) => {
 	const env_name_UNSAFE= req.params.env; 
 	const file_path_UNSAFE= req.params[0]; 
 	const src= req.body.src;
@@ -85,21 +67,19 @@ app.post('/src/:env/*', async (req, res) => {
 //S: EDITING FILE SERVER }
 
 //S: EDITING COMMANDS {
-app.get('/cmd/:env/*', async (req,res) => {
-	res.json({ //XXX:to config
-		"file": {
-			"git add": {},
-			"delete": {},
-		},
-		"env": {
-			"git commit": {},
-			"git push": {},
-		}
-	});
+app.get(CFG_API_PFX+'/cmd/:env/*', async (req,res) => {
+	const env_name_UNSAFE= req.params.env; 
+	const r= await api.env_commands({env_name_UNSAFE,''})
+	res.json(r);
 })
 
-app.post('/cmd/:env/*', async (req, res) => {
-	res.json({error: 'not implemented'}); //XXX:implement
+app.post(CFG_API_PFX+'/cmd/:env/*', async (req, res) => {
+	const env_name_UNSAFE= req.params.env; 
+	try {
+		const r= await api.env_command_execute({env_name_UNSAFE,req.body})
+		res.json(r);
+	} catch (ex) { res.json({error: ex+''}); }
+	res.json({error: 'not implemented'}); 
 });
 //S: EDITING COMMANDS }
 
